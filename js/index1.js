7bd23f50bb1e262b3517b7d4bac9a1d04287e579
unknown <html5G>
2020-05-20 19:15:09 +0800

    删除链表n

---
+++ js/index1.js
@@ -0,0 +1,17 @@
+var removeNthFromEnd = function(head, n) {
+    let preHead = new ListNode(0)
+    preHead.next = head
+    let fast = preHead, slow = preHead
+    // 快先走 n+1 步
+    while(n--) {
+        fast = fast.next
+    }
+    // fast、slow 一起前进
+    while(fast && fast.next) {
+        fast = fast.next
+        slow = slow.next
+    }
+    slow.next = slow.next.next
+    return preHead.next
+};
+removeNthFromEnd([1,2,3,4,5],2)
